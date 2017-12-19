c67450fe2f0650844cac56d3fe791f747c044524
arindamnayak-spigit <arindam.nayak@spigit.com>
2017-12-19 17:25:25 +0530

    fixed

--- on-success.sh
+++ on-success.sh
@@ -4,4 +4,5 @@ echo "on success"
 echo "branch " $TRAVIS_BRANCH
 echo "commit " $TRAVIS_COMMIT
 echo "pythong automerge.py $TRAVIS_BRANCH"
-python automerge.py $TRAVIS_BRANCH
+git clone https://github.com/arindamnayak/automatic-merge.git 
+cd automatic-merge && python automerge.py $TRAVIS_BRANCH
