afdf5fab31c1538fa2187203f7a08bf39fa650a3
Arindam Nayak <arindam.nayak@spigit.com>
2018-10-18 17:01:29 +0530

    new chang

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
-package com.spigit;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
-  public static void main(String[] args) {
+  private static final String SS = "s";
+  public static void main(String[] args) throws Exception  {
     System.out.println("Done");
+    FileReader fr = new FileReader(".");
+    Map<String, Integer> map = new HashMap<>();
+    map.put(null,2);
+
   }
 }
 
